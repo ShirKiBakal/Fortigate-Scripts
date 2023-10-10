5b86db93396859ff3c06ec6e64fe441a6de97263
ShirKiBakal <44676067+ShirKiBakal@users.noreply.github.com>
2023-10-10 12:29:06 +0300

    Add files via upload

---
+++ FortiGroups-Example.ps1
@@ -0,0 +1,30 @@
+﻿#variables
+$groupname = "Example"
+#csv name needs to be equal to groupname.
+#path also must be change to fit your environment
+$csv = import-csv -Path "C:\temp\$groupname.csv"
+$outfile = "C:\temp\$groupname.txt"
+$memeber=$null
+
+#add intial line
+Add-Content -Path $outfile "config firewall address"
+
+#creating addresses in a loop
+foreach ($line in $csv){
+Add-Content -Path $outfile "edit ""$($line.name)"""
+Add-Content -Path $outfile "set type $($line.type)"
+Add-Content -Path $outfile "set $($line.set) $($line.value)"
+Add-Content -Path $outfile "next"
+
+#string builder for group
+$memeber += "`"$($line.name)`" "
+}
+Add-Content -Path $outfile "end"
+
+
+#creating group from "$member"
+Add-Content -Path $outfile "config firewall addrgrp"
+Add-Content -Path $outfile "edit $groupname"
+Add-Content -Path $outfile "set member $memeber"
+Add-Content -Path $outfile "end"
+
