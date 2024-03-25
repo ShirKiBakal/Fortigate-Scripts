32f902d75a0b87daeade1f11b6117cd31c39a5c6
ShirKiBakal <44676067+ShirKiBakal@users.noreply.github.com>
2024-03-25 11:50:02 +0200

    Add files via upload

---
+++ fortiUsers.ps1
@@ -0,0 +1,17 @@
+﻿#variables
+$csv = import-csv -Path "C:\Users\shiranba\OneDrive - Essence Security\Fortigate Object Script - Users\users.csv"
+$outfile = "C:\Users\shiranba\OneDrive - Essence Security\Fortigate Object Script - Users\users.txt"
+
+#add intial line
+Add-Content -Path $outfile "config user local"
+
+#creating users in a loop
+foreach ($line in $csv){
+Add-Content -Path $outfile "edit ""$($line.name)"""
+Add-Content -Path $outfile "set passwd $($line.password)"
+Add-Content -Path $outfile "set two-factor email"
+Add-Content -Path $outfile "set email-to $($line.email)"
+Add-Content -Path $outfile "next"
+}
+Add-Content -Path $outfile "end"
+
