b398e118cc6f26574e6df2f53ca91baa0a02e392
ShirKiBakal <44676067+ShirKiBakal@users.noreply.github.com>
2024-03-25 12:00:18 +0200

    Update fortiUsers.ps1

--- fortiUsers.ps1
+++ fortiUsers.ps1
@@ -1,4 +1,4 @@
-﻿#variables
+#variables
 $csv = import-csv -Path "C:\Users\shiranba\OneDrive - Essence Security\Fortigate Object Script - Users\users.csv"
 $outfile = "C:\Users\shiranba\OneDrive - Essence Security\Fortigate Object Script - Users\users.txt"
 
@@ -11,6 +11,16 @@ Add-Content -Path $outfile "edit ""$($line.name)"""
 Add-Content -Path $outfile "set passwd $($line.password)"
 Add-Content -Path $outfile "set two-factor email"
 Add-Content -Path $outfile "set email-to $($line.email)"
+Add-Content -Path $outfile "next"
+}
+Add-Content -Path $outfile "end"
+
+#add intial line
+Add-Content -Path $outfile "config user group"
+
+foreach ($line in $csv){
+Add-Content -Path $outfile "edit ""$($line.group)"""
+Add-Content -Path $outfile "append member $($line.name)"
 Add-Content -Path $outfile "next"
 }
 Add-Content -Path $outfile "end"
